ea95fead66144b927870c7ca3e9e319baa286b08
Juksefantomet <Juksefantomet@hotmail.com>
2017-08-30 17:16:49 +0200

    Added server/client feature, connection is working but stopping the loop. needs attention

--- main.rb
+++ main.rb
@@ -1,16 +1,22 @@
 require 'gosu'
+require 'socket'
 
 class Game < Gosu::Window
-	attr_accessor :width
+	attr_accessor :width, :heigth
 	def initialize
 		@width = 1024
 		@heigth = 768
 		super(width, heigth, false)
 		self.caption = "Life in the box - client"
+		@s = TCPSocket.new('localhost', 18028)
 	end
 	def button_down(id)
 		case id
 		when Gosu::KbEscape then close
+		when Gosu::KbK then
+			line = @s.gets
+			puts line
+		#	close
 		end
 	end
 	def needs_cursor?
--- server.rb
+++ server.rb
@@ -1,12 +1,14 @@
 require 'gosu'
+require 'socket'
 
 class Game < Gosu::Window
-	attr_accessor :width
+	attr_accessor :width, :heigth
 	def initialize
 		@width = 1024
 		@heigth = 768
 		super(width, heigth, false)
 		self.caption = "Server - Life in the box"
+		@server = TCPServer.new 18028
 	end
 	def button_down(id)
 		case id
@@ -20,7 +22,11 @@ class Game < Gosu::Window
 
 	end
 	def draw
-
+		#Thread.start(@server.accept) do |client|
+		client = @server.accept
+		client.puts(Time.now.ctime + " Saying hello")
+		client.close
+	#end		
 	end
 end
 
